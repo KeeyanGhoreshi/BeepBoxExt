e7d4ac533cd8cb6cedf89fa1356f069190f1a506
Keeyan Ghoreshi <kghoreshi@mitre.org>
2019-01-01 20:22:17 -0500

    basics

---
+++ contentScript.js
@@ -0,0 +1,3 @@
+console.log("henlo");
+var url = window.location.href;
+console.log(doc);
---
+++ inject.js
@@ -0,0 +1,23 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+    var pepe = "yu fdum";
+	// just place a div at top right
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+
+    alert('inserted self');
+    console.log("henlo");
+    console.log(beepbox);
+
+    console.log(this._doc);
+    console.log(_doc);
+    console.log(beepbox);
+
+})();
+
